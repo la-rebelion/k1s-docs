575bce00b888891f3140c2e91e74926046dc7395
Adrian Escutia Soto <adrianescutia@yahoo.com.mx>
2024-03-18 07:47:04 -0500

    Docusaurus v3 is interpreting the {X} as a variable, need escape

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -23,10 +23,10 @@ const config = {
   // Even if you don't use internalization, you can use this field to set useful
   // metadata like html lang. For example, if your site is Chinese, you may want
   // to replace "en" with "zh-Hans".
-  i18n: {
-    defaultLocale: 'en',
-    locales: ['en', 'es'],
-  },
+  // i18n: {
+  //   defaultLocale: 'en',
+  //   locales: ['en', 'es'],
+  // },
 
   presets: [
     [
